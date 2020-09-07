b13099c0bc71b576d666ed851cb5aff8bd8945d5
Chrome1278 <63706125+Chrome1278@users.noreply.github.com>
2020-09-07 11:58:38 +0300

    Add files via upload

---
+++ PracticeTasks2/src/TestAuthor.java
@@ -0,0 +1,8 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Ben", "BenA@gmail.com", 'm');
+        Author a2 = new Author("Kate", "KateK@gmail.com", 'f');
+        System.out.println(a1 + "\n");
+        System.out.println(a2);
+    }
+}
